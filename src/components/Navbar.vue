71f3f85f3bcbe7580a3055bd2db70f20bfc90f69
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-02-13 20:55:03 +0200

    navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <div class="main-container">
+      <div class="header-content-wrapper">
+        <div class="logo-wrapper"><img src="https://via.placeholder.com/150x50/fff/?text=FSOnlineStore" alt="logo" class="logo"></div>
+          <nav class="navbar">
+            <router-link to="/" class="nav-item">Home</router-link>
+            <router-link to="/about" class="nav-item">About</router-link>
+            <router-link to="/shop" class="nav-item">Shop</router-link>
+            <router-link to="/lookbook" class="nav-item">LookBook</router-link>
+            <router-link to="/contact" class="nav-item">Contact</router-link> 
+          </nav>
+          <button class="btn">Cart &nbsp;<i class="fa-light fa-cart-shopping"></i></button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
