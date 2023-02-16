9e5145ae56f084ca07f54376599296a7eb44f0d6
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-02-16 21:24:45 +0200

    added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,32 @@
 <template>
   <Navbar />
   <div class="about">
-    <h1 class="heading">This is an about page</h1>
+    <div class="main-container">
+      <div class="about-content-section">
+        <figure>
+          <img src="https://via.placeholder.com/1152x550" alt="about-img" class="img about-img">
+          <figcaption>Meet the team</figcaption>
+        </figure>
+      </div>
+      <div class="about-content-section">
+        <h3 class="secondary-heading">About Us</h3>
+        <p class="content-paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet fugiat cum alias at, dolorem asperiores delectus possimus aspernatur ullam, minima aut quod quae autem voluptatum eius sint reprehenderit porro doloremque voluptate dicta voluptatibus. Debitis repellat aliquam neque molestiae voluptates beatae?</p>
+        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam ipsam sint ut, odit eius. Atque incidunt veritatis, fugiat debitis quae natus quas optio molestiae commodi porro, suscipit impedit voluptas aliquid illo animi autem error dicta perferendis velit cum eligendi!</p>
+        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quibusdam natus vel culpa, officiis voluptas accusamus rem. Possimus atque non est, eius deleniti ut perferendis rem incidunt quasi consequuntur distinctio minus quo ea suscipit velit. Impedit maiores officia repellendus porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, vel fugiat? Illum exercitationem explicabo veniam, molestias repellendus illo maiores eligendi impedit velit vel corrupti accusamus nobis perspiciatis eum sed provident similique ipsa sit facilis aliquid fugit! Aliquid id culpa explicabo unde molestias. Atque possimus, accusantium voluptatem autem vitae cupiditate praesentium!
+          
+        </p>
+      </div>
+      <div class="about-content-section">
+        <h3 class="secondary-heading">The Team</h3>
+        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quibusdam natus vel culpa, officiis voluptas accusamus rem. Possimus atque non est, eius deleniti ut perferendis rem incidunt quasi consequuntur distinctio minus quo ea suscipit velit. Impedit maiores officia repellendus porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, vel fugiat? Illum exercitationem explicabo veniam, molestias repellendus illo maiores eligendi impedit velit vel corrupti accusamus nobis perspiciatis eum sed provident similique ipsa sit facilis aliquid fugit! Aliquid id culpa explicabo unde molestias. Atque possimus, accusantium voluptatem autem vitae cupiditate praesentium!
+        </p>
+      </div>
+      <div class="about-content-section">
+        <h3 class="secondary-heading">Mission & Vision</h3>
+        <p class="content-paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet fugiat cum alias at, dolorem asperiores delectus possimus aspernatur ullam, minima aut quod quae autem voluptatum eius sint reprehenderit porro doloremque voluptate dicta voluptatibus. Debitis repellat aliquam neque molestiae voluptates beatae?</p>
+        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam ipsam sint ut, odit eius. Atque incidunt veritatis, fugiat debitis quae natus quas optio molestiae commodi porro, suscipit impedit voluptas aliquid illo animi autem error dicta perferendis velit cum eligendi!</p>
+      </div>
+    </div>
   </div>
 </template>
 
