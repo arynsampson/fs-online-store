07a8564fe60475e552fed0074d9cad9b9ea56c8c
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-02-18 21:57:00 +0200

    add mock data to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,14 @@
       <div class="main-container">
         <div class="hero-content-wrapper">
           <div class="hero-items-wrapper">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            <Item 
+              v-for="(item, index) in data"
+              :key="index"
+              :uid="item.id"
+              :image="item.item_image"
+              :name="item.item_name"
+              :price="item.item_price"
+            />
           </div>
         </div>
       </div>
@@ -17,21 +20,27 @@
     
     <div class="main-container">
       <h3 class="secondary-heading">Featured</h3>
-        <div class="content-section">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+        <div class="content-section overflow-x-scroll">
+          <Item 
+              v-for="(item, index) in featuredItems"
+              :key="index"
+              :uid="item.id"
+              :image="item.item_image"
+              :name="item.item_name"
+              :price="item.item_price"
+            />
         </div>
         <br><hr />
       <h3 class="secondary-heading">New Additions</h3>
-        <div class="content-section">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+        <div class="content-section overflow-x-scroll">
+          <Item 
+              v-for="(item, index) in data"
+              :key="index"
+              :uid="item.id"
+              :image="item.item_image"
+              :name="item.item_name"
+              :price="item.item_price"
+            />
         </div>
         <br>
     </div>
@@ -41,7 +50,14 @@
 
 <script setup>
 // @ is an alias to /src
+import { reactive, computed, ref } from 'vue'
 import Navbar from '@/components/Navbar.vue'
 import Item from '@/components/Item.vue'
+import MockData from '@/mock/mock-data.json'
 
+const data = ref(MockData);
+
+const featuredItems = computed(() => {
+  return data.value.filter((item) => item.isFeatured );
+})
 </script>
