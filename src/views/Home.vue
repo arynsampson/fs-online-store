31f278f74e33e94a3a3f4f2dc920466bb21d1664
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-02-19 18:10:09 +0200

    implement swiperjs carousels on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,25 @@
       <div class="main-container">
         <div class="hero-content-wrapper">
           <div class="hero-items-wrapper">
-            <Item 
-              v-for="(item, index) in data"
-              :key="index"
-              :uid="item.id"
-              :image="item.item_image"
-              :name="item.item_name"
-              :price="item.item_price"
-            />
+            <swiper 
+              :slides-per-view="4"
+              :pagination="{
+                clickable: true
+              }"
+              :navigation="true"
+              :modules="modules"
+              class="mySwiper"
+            >
+              <swiper-slide v-for="(item, index) in data" :key="index">
+                <Item 
+                  :key="index"
+                  :uid="item.id"
+                  :image="item.item_image"
+                  :name="item.item_name"
+                  :price="item.item_price"
+                />
+              </swiper-slide>
+            </swiper>
           </div>
         </div>
       </div>
@@ -20,17 +31,28 @@
     
     <div class="main-container">
       <h3 class="secondary-heading">Featured</h3>
-        <div class="content-section overflow-x-scroll">
-          <Item 
-              v-for="(item, index) in featuredItems"
-              :key="index"
-              :uid="item.id"
-              :image="item.item_image"
-              :name="item.item_name"
-              :price="item.item_price"
-            />
+        <div class="content-section">
+          <swiper 
+              :slides-per-view="4"
+              :pagination="{
+                clickable: true
+              }"
+              :navigation="true"
+              :modules="modules"
+              class="mySwiper"
+            >
+              <swiper-slide v-for="(item, index) in featuredItems" :key="index">
+                <Item 
+                  :key="index"
+                  :uid="item.id"
+                  :image="item.item_image"
+                  :name="item.item_name"
+                  :price="item.item_price"
+                />
+              </swiper-slide>
+            </swiper>
         </div>
-        <br><hr />
+        <br><hr /><!--
       <h3 class="secondary-heading">New Additions</h3>
         <div class="content-section overflow-x-scroll">
           <Item 
@@ -41,23 +63,48 @@
               :name="item.item_name"
               :price="item.item_price"
             />
-        </div>
+        </div>-->
         <br>
-    </div>
+    </div> 
 
   </div>
 </template>
 
-<script setup>
+<script>
 // @ is an alias to /src
 import { reactive, computed, ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, Pagination, Navigation } from 'swiper'
 import Navbar from '@/components/Navbar.vue'
 import Item from '@/components/Item.vue'
 import MockData from '@/mock/mock-data.json'
+import 'swiper/css';  
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-const data = ref(MockData);
+export default {
+  components: {
+      Navbar,
+      Item,
+      Swiper,
+      SwiperSlide,
+  },
+  setup() {
+    const data = ref(MockData);
 
-const featuredItems = computed(() => {
-  return data.value.filter((item) => item.isFeatured );
-})
+    const featuredItems = computed(() => {
+    return data.value.filter((item) => item.isFeatured );
+    })
+
+    return {
+      Navbar,
+      Item,
+      data,
+      Swiper,
+      SwiperSlide,
+      featuredItems,
+      modules: [Autoplay, Pagination, Navigation]
+    }
+  }
+} 
 </script>
