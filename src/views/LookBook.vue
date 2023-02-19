d8376650ace8c64d34efbdf8224d69d98088252e
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-02-19 18:10:23 +0200

    display individual items on lookbook page

--- src/views/LookBook.vue
+++ src/views/LookBook.vue
@@ -1,11 +1,43 @@
 <template>
   <Navbar />
     <div class="lookbook">
-      <h1>This is the lookbook page</h1>
+      <div class="main-container">
+        <p><img :src="item.item_image" alt=""></p>
+        <p>{{ item.item_name }}</p>
+        <p>{{ item.item_price }}</p>
+      </div>
     </div>
 </template>
   
-<script setup>
+<script>
 // @ is an alias to /src
+import { onMounted } from 'vue'
 import Navbar from '@/components/Navbar.vue'
+import MockData from '@/mock/mock-data.json'
+
+export default {
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      data: [],
+      item: {}
+    }
+  },  
+  setup() {
+
+    return {
+      Navbar
+    }
+  },
+  mounted() {
+    this.data = MockData
+    this.data.forEach((item) => {
+      if(item.id == this.$route.params.id) {
+        this.item = item;
+      }
+    })
+  }
+}
 </script>
