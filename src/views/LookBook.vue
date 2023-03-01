a4ed18afd5e941f49c4bd43210dd8d727d373867
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-03-01 22:08:24 +0200

    change code to composition api

--- src/views/LookBook.vue
+++ src/views/LookBook.vue
@@ -2,11 +2,9 @@
   <Navbar />
     <div class="lookbook">
       <div class="main-container">
-        <p><img :src="item.image" alt=""></p>
+        <p><img :src="item.image" :alt="carMake"></p>
         <p>{{ item.carMake }} {{ item.carModel }}</p>
         <p>R{{ item.price }}</p>
-        
-
         <button v-if="!inCart" @click="addToCart">Add to cart</button>
         <p v-else>Item is in cart</p>
       </div>
@@ -15,70 +13,56 @@
   
 <script>
 // @ is an alias to /src
-import { onMounted } from 'vue'
+import { onMounted, ref, reactive } from 'vue'
+import { useRoute } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 
 export default {
   components: {
     Navbar
   },
-  data() {
-    return {
-      data: [],
-      item: {},
-      inCart: false
-    }
-  },  
   setup() {
 
-    return {
-      Navbar
-    }
-  },
-  methods: {
-    addToCart() {
+    const item = reactive({});
+    const inCart = ref(false);
+    const route = useRoute();
+    const msg = ref('');
+
+    onMounted(() => {
+      const cars = JSON.parse(localStorage.getItem('cars'));
+        for(let x = 0; x < cars.length; x++) {
+          if(cars[x].id == route.params.id) {
+            item.value = cars[x];
+          }
+        }
+
       const cart = JSON.parse(localStorage.getItem('cart'));
-    
-        cart.push({
-          itemID: this.item.id,
-          itemName: this.item.carMake,
-          itemPrice: this.item.price,
-          itemImage: this.item.image,
-          quantity: 1
-        });
+        for(let x = 0; x < cart.length; x++) {
+          if(cart[x].id == item.id) {
+            inCart.value = true;
+          }
+        }
+    });
 
-      // for(let x = 0; x < cart.length; x++) {
-      //   if(cart[x].itemID === parseInt(this.$route.params.id)) {
-      //       alert('You already have this item in your cart.');
-      //   } else {
-      //       // cart.push({
-      //       // itemID: this.item.id,
-      //       // itemName: this.item.item_name,
-      //       // itemPrice: this.item.item_price,
-      //       // itemImage: this.item.item_image,
-      //       // quantity: 1
-      //       // });
-      //   }
-      // }
-
-      this.inCart = true;
-
+    const addToCart = () => {
+      const cart = JSON.parse(localStorage.getItem('cart'));
+      cart.push({
+        id: item.id,
+        carMake: item.carMake,
+        carModel: item.carModel,
+        price: item.price,
+        image: item.image,
+        quantity: 1
+      });
+      inCart.value = true;
       localStorage.setItem('cart', JSON.stringify(cart));
     }
-  },
-  mounted() {
-    const cars = JSON.parse(localStorage.getItem('cars'));
-    cars.forEach((item) => {
-      if(item.id == this.$route.params.id) {
-        this.item = item;
-      }
-    })
 
-    const cart = JSON.parse(localStorage.getItem('cart'));
-    for(let x = 0; x < cart.length; x++) {
-      if(cart[x].itemID === this.item.id) {
-        this.inCart = true;
-      }
+    return {
+      item,
+      inCart,
+      msg,
+      addToCart
     }
   }
 }
