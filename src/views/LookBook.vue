10247fbbca4454e0b72c3293fa4bd43d9556cebc
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-03-16 21:09:56 +0200

    lookbook page restyle

--- src/views/LookBook.vue
+++ src/views/LookBook.vue
@@ -2,18 +2,25 @@
   <Navbar />
     <div class="lookbook">
       <div class="main-container">
-        <p><img :src="item.image" :alt="carMake"></p>
-        <p>{{ item.carMake }} {{ item.carModel }}</p>
-        <p>R{{ item.price }}</p>
-        <button v-if="!inCart" @click="addToCart">Add to cart</button>
-        <p v-else>Item is in cart</p>
+        <div>
+        <img :src="item.value.image" :alt="item.value.carMake">
+        </div>
+        <div>
+          <section>
+            <h2>{{ item.value.carModelYear }} {{ item.value.carMake }} {{ item.value.carModel }}</h2>
+            <p>R{{ item.value.price }}</p>
+            <p>Colour: <span class="colour-block" :style="{background: item.value.colour}"></span>{{ item.value.colour }}</p>
+          </section>
+          <button v-if="!inCart" @click="addToCart">Add to cart</button>
+          <p v-else>Item is in cart</p>
+        </div>
       </div>
     </div>
 </template>
   
 <script>
 // @ is an alias to /src
-import { onMounted, ref, reactive } from 'vue'
+import { onBeforeMount, ref, reactive } from 'vue'
 import { useRoute } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 
@@ -21,6 +28,7 @@ export default {
   components: {
     Navbar
   },
+  
   setup() {
 
     const item = reactive({});
@@ -28,7 +36,7 @@ export default {
     const route = useRoute();
     const msg = ref('');
 
-    onMounted(() => {
+    onBeforeMount(() => {
       const cars = JSON.parse(localStorage.getItem('cars'));
         for(let x = 0; x < cars.length; x++) {
           if(cars[x].id == route.params.id) {
@@ -47,15 +55,16 @@ export default {
     const addToCart = () => {
       const cart = JSON.parse(localStorage.getItem('cart'));
       cart.push({
-        id: item.id,
-        carMake: item.carMake,
-        carModel: item.carModel,
-        price: item.price,
-        image: item.image,
+        id: item.value.id,
+        carMake: item.value.carMake,
+        carModel: item.value.carModel,
+        price: item.value.price,
+        image: item.value.image,
         quantity: 1
       });
       inCart.value = true;
       localStorage.setItem('cart', JSON.stringify(cart));
+
       alert(`You have added: ${item.value.carMake} ${item.value.carModel} to your cart.`);
     }
 
@@ -68,3 +77,65 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.lookbook {
+  height: 100vh;
+}
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+  }
+
+  .main-container img {
+    width: 500px;
+    height: 200px;
+  }
+  .main-container div:last-child {
+    text-align: left;
+    width: 100%;
+    height: 400px;
+    padding-left: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+
+  h2 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  section p {
+    margin-top: 20px;
+    font-size: 18px;
+  }
+
+  button {
+    padding: 10px;
+    width: 300px;
+    background-color: coral;
+    border-radius: 10px;
+    color: #fff;
+  }
+
+@media (min-width:768px) {
+  .main-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .main-container div:first-child {
+    width: 50%;
+  }
+  .main-container div:last-child {
+    width: 40%;
+  }
+
+}
+
+</style>
